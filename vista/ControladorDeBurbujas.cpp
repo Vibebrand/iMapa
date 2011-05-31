12e833b010e00241f1a9284006870424bced1c14
Luis Alejandro Rangel Sánchez <luis.rangel@inegi.org.mx>
2011-05-31 16:28:57 -0500

    Mock funcional, falta error en memoria

--- vista/ControladorDeBurbujas.cpp
+++ vista/ControladorDeBurbujas.cpp
@@ -9,25 +9,35 @@ ControladorDeBurbujas::ControladorDeBurbujas(QObject *parent):
 
 void ControladorDeBurbujas::agregarBurbujasAlMapa()
 {
-    _entidadesFederativasActivaas = _servicioInformacionEstadistica->obtenerPeriodo(1);
+    /*_entidadesFederativasActivaas = _servicioInformacionEstadistica->obtenerPeriodo(1);
     periodoEstadisticoActivo=1;
     qreal radioMaximo = 100;
     int numeroTotalDePoblacion  = _servicioInformacionEstadistica->obtenerTotalDePoblacionPorPeriodo();
     foreach(EntidadFederativa entidad, (*_entidadesFederativasActivaas))
-    {
+    {*/
         Burbuja burbuja;
-        burbuja.nombre = entidad.nombre;
+        burbuja.nombre = "burbuja";
+        burbuja.color = Qt::red;
+        burbuja.latitud = -102.3;
+        burbuja.longitiud = 21.883333;
+        burbuja.radio = 100;
+
+        /*burbuja.nombre = entidad.nombre;
         burbuja.latitud =entidad.latitud;
         burbuja.longitiud=entidad.longitud;
         qreal porcentajePorEntidad = (entidad.totalDePoblacion * 100)/numeroTotalDePoblacion;
-        burbuja.radio = (porcentajePorEntidad * radioMaximo )/ 100 ;
+        burbuja.radio = (porcentajePorEntidad * radioMaximo )/ 100 ;*/
         if(_controladorPluginBurbujas)
+        {
+            qDebug() << "ControladorDeBurbujas::agregarBurbujasAlMapa() .. if(_controladorPluginBurbujas)";
             _delegadosObjetoBurbuja[burbuja.nombre] = _controladorPluginBurbujas->agregarElemento(burbuja);
-    }
+        }
+    //}
 }
 
 void ControladorDeBurbujas::elementoSeleccionado(QString nombre)
 {
+    qDebug() << nombre;
     foreach(EntidadFederativa entidad, (*_entidadesFederativasActivaas))
         if(entidad.nombre.compare(nombre))
         {
