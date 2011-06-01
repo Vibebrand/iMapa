372ae2b7eb37c70230718f3612a0802dcc2416e0
Luis Alejandro Rangel Sánchez <luis.rangel@inegi.org.mx>
2011-06-01 13:24:18 -0500

    Version inicial

--- servicio/ServicioInformacionEstadistica.cpp
+++ servicio/ServicioInformacionEstadistica.cpp
@@ -1,6 +1,21 @@
 #include "servicio/ServicioInformacionEstadistica.h"
 
-ServicioInformacionEstadistica::ServicioInformacionEstadistica(){}
+#include <QDebug>
+#include <QVariant>
+#include <QSqlDatabase>
+#include <QSqlQuery>
+#include <QSqlRecord>
+
+void obtenerElementos();
+
+ServicioInformacionEstadistica::ServicioInformacionEstadistica()
+{
+    //obtenerElementos();
+}
+
+ServicioInformacionEstadistica::~ServicioInformacionEstadistica()
+{
+}
 
 int ServicioInformacionEstadistica::obtenerPeriodos()
 {
@@ -17,3 +32,29 @@ qreal ServicioInformacionEstadistica::obtenerTotalDePoblacionPorPeriodo()
     return 0.0;
 }
 
+void obtenerElementos()
+{
+    qDebug() << "obtenerElementos";
+
+    QSqlDatabase baseDatos = QSqlDatabase::addDatabase("QSQLITE");
+    baseDatos.setDatabaseName("info.sqlite");
+    baseDatos.setHostName("localhost");
+    if(baseDatos.open())
+    {
+        qDebug() << "obtenerElementos::open";
+
+        QSqlQuery query("SELECT * FROM mena;");
+
+        int entidad = query.record().indexOf("entidad");
+        int grupo = query.record().indexOf("grupo");
+
+        query.exec();
+        while(query.next())
+        {
+            qDebug() << query.value(entidad).toInt() << query.value(grupo).toString();
+        }
+
+        baseDatos.close();
+        qDebug() << "obtenerElementos::close";
+    }
+}
--- servicio/ServicioInformacionEstadistica.h
+++ servicio/ServicioInformacionEstadistica.h
@@ -1,14 +1,20 @@
 #ifndef SERVICIOINFORMACIONESTADISTICA_H
 #define SERVICIOINFORMACIONESTADISTICA_H
 #include "IServicioInformacionEstadistica.h"
+#include <QMap>
+
 
 class ServicioInformacionEstadistica : public IServicioInformacionEstadistica
 {
 public:
     ServicioInformacionEstadistica();
+    ~ServicioInformacionEstadistica();
     int obtenerPeriodos();
     QList<EntidadFederativa>* obtenerPeriodo(int periodo);
     qreal obtenerTotalDePoblacionPorPeriodo();
+
+
+private:
 };
 
 
